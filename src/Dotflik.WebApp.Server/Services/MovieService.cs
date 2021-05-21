974df516fe874372d9e5d3d41170a47822125888
Huy Ngo <huybngo@yahoo.com>
2021-05-20 18:30:04 -0700

    Add grpc server project
    - Update package proto path
    - Link to google protobuf file in the server project as link

---
+++ src/Dotflik.WebApp.Server/Services/MovieService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Dotflik.Protobuf.Movie;
+
+namespace Dotflik.Web.Server.Services
+{
+  public class MovieService : Protobuf.Movie.MovieService.MovieServiceBase
+  {
+    public override async Task GetMovies(Empty request, IServerStreamWriter<Movie> responseStream, ServerCallContext context)
+    {
+      for (int i = 0; i < 5; i++)
+      {
+        await Task.Delay(1000);
+        var response = new Movie { Id = (i+1).ToString(), Year = 2021 + i + 1};
+        await responseStream.WriteAsync(response);
+      }
+    }
+
+
+  }
+}
---
+++ src/Dotflik.WebApp.Server/Startup.cs
@@ -0,0 +1,51 @@
+using Dotflik.Web.Server.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+
+namespace Dotflik.Web.Server
+{
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddGrpc();
+      services.AddGrpcReflection();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseRouting();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapGrpcService<MovieService>();
+
+        endpoints.MapGet("/", async context =>
+        {
+            await context.Response.WriteAsync("Only gRPC is supported");
+        });
+
+        if (env.IsDevelopment())
+        {
+          endpoints.MapGrpcReflectionService();
+        }
+
+      });
+
+      
+    }
+  }
+}
