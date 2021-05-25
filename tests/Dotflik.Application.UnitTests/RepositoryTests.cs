5cfd0f6845f4a897f91be3333cb2448adda72878
Huy Ngo <huybngo@yahoo.com>
2021-05-24 19:57:01 -0700

    Add application and infrastructure unit tests

---
+++ tests/Dotflik.Application.UnitTests/RepositoryTests.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Reflection;
+using Dotflik.Application.Repositories;
+using Dotflik.Application.Repositories.Settings;
+using Xunit;
+using Moq;
+
+namespace Dotflik.Application.UnitTests
+{
+  public class RepositoryTests
+  {
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    [InlineData(" ")]
+    public void Constructor_EmptyConnectionString_ThrowsArgumentException(string connectionString)
+    {
+      var dbSettingsMock = new Mock<IDatabaseSettings>();
+      dbSettingsMock.SetupGet(m => m.ConnectionString).Returns(connectionString);
+
+      // Outer exception is thrown by Xunit to assert the inner exception instead
+      var ex = Assert.Throws<TargetInvocationException>(
+        () => new Mock<Repository>(dbSettingsMock.Object).Object);
+
+      Assert.IsType<ArgumentException>(ex.InnerException);
+    }
+
+    [Theory]
+    [InlineData("  x  ")]
+    [InlineData("x")]
+    [InlineData("x ")]
+    [InlineData("x 0")]
+    public void Constructor_NonEmptyConnectionString_RepositoryCanBeConstructed(string connectionString)
+    {
+      var dbSettingsMock = new Mock<IDatabaseSettings>();
+      dbSettingsMock.SetupGet(m => m.ConnectionString).Returns(connectionString);
+
+      _ = new Mock<Repository>(dbSettingsMock.Object).Object;
+    }
+
+  }
+}
---
+++ tests/Dotflik.Infrastructure.UnitTests/RepositoryServiceTests.cs
@@ -0,0 +1,43 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+using Dotflik.Application.Repositories;
+using Dotflik.Application.Repositories.Settings;
+
+using Xunit;
+using Moq;
+
+namespace Dotflik.Infrastructure.UnitTests
+{
+  public class RepositoryServiceTests
+  {
+    [Fact]
+    public void AddMovieRepository_SupportDatabase_MovieRepositoryIsAdded()
+    {
+      // Mock a dummy database settings
+      var dbSettingsMock = new Mock<IDatabaseSettings>();
+      dbSettingsMock.SetupGet(m => m.ConnectionString).Returns("dummy");
+
+      var services = new ServiceCollection();
+      services.AddMovieRepository(Database.PostgresSQL);
+      services.AddSingleton(dbSettingsMock.Object);
+      var provider = services.BuildServiceProvider();
+
+      // No need to call any Xunit.Assert because GetRequiredService
+      // would throw an exception if the service is not registered
+      _ = provider.GetRequiredService<IMovieRepository>();
+    }
+
+    [Fact]
+    public void AddMovieRepository_NotSupportDatabase_ThrowsNotSupportedException()
+    {
+      // Use an arbitrarily number that is not in the enum to "fake" a not supported db
+      var notSupportedDb = (Database)int.MaxValue;
+      var services = new ServiceCollection();
+
+      Assert.Throws<NotSupportedException>(() => services.AddMovieRepository(notSupportedDb));     
+    }
+
+
+  }
+}
