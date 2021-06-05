7ce6cf2762a5c086259028abc6efbcb0e01b6f7d
Huy Ngo <huybngo@yahoo.com>
2021-06-05 15:32:30 -0700

    Add blazor webassembly project

---
+++ src/Dotflik.WebApp.Client/Program.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+
+using Dotflik.Protobuf.Movie;
+
+namespace Dotflik.WebApp.Client
+{
+  public class Program
+  {
+    public static async Task Main(string[] args)
+    {
+      var builder = WebAssemblyHostBuilder.CreateDefault(args);
+      builder.RootComponents.Add<App>("#app");
+
+      builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+      builder.Services.AddSingleton(sp =>
+      {
+        var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions
+        {
+          HttpHandler = new GrpcWebHandler(new HttpClientHandler())
+        });
+
+        return new MovieService.MovieServiceClient(channel);
+      });
+
+      await builder.Build().RunAsync();
+    }
+  }
+}
