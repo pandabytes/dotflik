c43ca291a17b17ed058ae227ffefb0b13cdb3556
Huy Ngo <huybngo@yahoo.com>
2021-06-11 23:46:26 -0700

    Fix unit tests because of the new aggregate movie class

--- tests/Dotflik.WebApp.Server.UnitTests/Services/MovieServiceTests.cs
+++ tests/Dotflik.WebApp.Server.UnitTests/Services/MovieServiceTests.cs
@@ -7,8 +7,9 @@ using Microsoft.Extensions.Logging;
 using Grpc.Core;
 using Google.Protobuf.WellKnownTypes;
 
+using Dotflik.Protobuf;
 using Dotflik.Protobuf.Pagination;
-using Dotflik.Protobuf.Movie;
+using Dotflik.Protobuf.Resources;
 using Dotflik.Application.Repositories;
 using Dotflik.WebApp.Server.Mappings;
 
@@ -20,6 +21,11 @@ namespace Dotflik.WebApp.Server.Services.UnitTests
 {
   public class MovieServiceTests
   {
+    /// <summary>
+    /// Random seed to generate fake data
+    /// </summary>
+    private const int RandomSeed = 8675309;
+
     /// <summary>
     /// Object under test
     /// </summary>
@@ -54,7 +60,7 @@ namespace Dotflik.WebApp.Server.Services.UnitTests
     public async Task GetMovieById_MovieNotFound_ThrowsRpcExceptionWithInvalidArgument()
     {
       // Arrange
-      var notFoundMovieTask = Task.FromResult<Domain.Entities.Movie?>(null);
+      var notFoundMovieTask = Task.FromResult<Domain.Aggregates.Movie?>(null);
       var request = new GetMovieByIdRequest { Id = string.Empty };
 
       m_movieRepositoryMock.Setup(m => m.GetByIdAsync(It.IsAny<string>()))
@@ -69,9 +75,9 @@ namespace Dotflik.WebApp.Server.Services.UnitTests
     public async Task GetMovieById_MovieFound_ReturnsProtobufMovie()
     {
       // Arrange
-      var movie = GenerateFakeMovies(1)[0];
+      var movie = GenerateFakeMovieAggrs(1)[0];
       var expectProtobufMovie = movie.ToProtobuf();
-      var movieTask = Task.FromResult<Domain.Entities.Movie?>(movie);
+      var movieTask = Task.FromResult<Domain.Aggregates.Movie?>(movie);
       var request = new GetMovieByIdRequest { Id = string.Empty };
 
       m_movieRepositoryMock.Setup(m => m.GetByIdAsync(It.IsAny<string>()))
@@ -85,37 +91,23 @@ namespace Dotflik.WebApp.Server.Services.UnitTests
     }
 
     [Fact]
-    public async Task GetMovieByTitle_MovieNotFound_ThrowsRpcExceptionWithInvalidArgument()
-    {
-      // Arrange
-      var notFoundMovieTask = Task.FromResult<Domain.Entities.Movie?>(null);
-      var request = new GetMovieByTitleRequest { Title = string.Empty };
-
-      m_movieRepositoryMock.Setup(m => m.GetByTitleAsync(It.IsAny<string>()))
-                           .Returns(notFoundMovieTask);
-
-      // Act and Assert
-      await Assert.ThrowsAsync<ArgumentException>(
-        () => m_movieService.GetMovieByTitle(request, m_serverContext));
-    }
-
-    [Fact]
     public async Task GetMovieByTitle_MovieFound_ReturnsProtobufMovie()
     {
       // Arrange
-      var movie = GenerateFakeMovies(1)[0];
-      var expectProtobufMovie = movie.ToProtobuf();
-      var movieTask = Task.FromResult<Domain.Entities.Movie?>(movie);
+      var movies = GenerateFakeMovieAggrs(5);
+      var expectProtobufMovies = movies.Select(m => m.ToProtobuf());
+      var movieTask = Task.FromResult<IEnumerable<Domain.Aggregates.Movie>>(movies);
       var request = new GetMovieByTitleRequest { Title = string.Empty };
 
       m_movieRepositoryMock.Setup(m => m.GetByTitleAsync(It.IsAny<string>()))
                            .Returns(movieTask);
 
       // Act
-      var actualProtobufMovie = await m_movieService.GetMovieByTitle(request, m_serverContext);
+      var response = await m_movieService.GetMovieByTitle(request, m_serverContext);
+      var actualProtobufMovie = response.Movies;
 
       // Assert
-      Assert.Equal(expectProtobufMovie, actualProtobufMovie);
+      Assert.Equal(expectProtobufMovies, actualProtobufMovie);
     }
 
     [Fact]
@@ -125,11 +117,11 @@ namespace Dotflik.WebApp.Server.Services.UnitTests
       // Add 10 number of movies so that we know
       // we would only get at most m_maxPageSize movies,
       // which is a subset of all the movies
-      var movies = GenerateFakeMovies(m_maxPageSize + 10);
+      var movies = GenerateFakeMovieAggrs(m_maxPageSize + 10);
 
       var request = new PaginationRequest { PageSize = 0, PageToken = string.Empty };
 
-      var expectMovies = (IEnumerable<Domain.Entities.Movie>) movies.GetRange(0, m_maxPageSize);
+      var expectMovies = (IEnumerable<Domain.Aggregates.Movie>) movies.GetRange(0, m_maxPageSize);
       var expectMoviesTask = Task.FromResult(expectMovies);
       m_movieRepositoryMock.Setup(m => m.GetAllAsync(m_maxPageSize, It.IsAny<int>()))
                            .Returns(expectMoviesTask);
@@ -148,11 +140,11 @@ namespace Dotflik.WebApp.Server.Services.UnitTests
       // Add 10 number of movies so that we know
       // we would only get at most m_maxPageSize movies,
       // which is a subset of all the movies
-      var movies = GenerateFakeMovies(m_maxPageSize + 10);
+      var movies = GenerateFakeMovieAggrs(m_maxPageSize + 10);
 
       var request = new PaginationRequest { PageSize = m_maxPageSize + 10, PageToken = string.Empty };
 
-      var expectMovies = (IEnumerable<Domain.Entities.Movie>)movies.GetRange(0, m_maxPageSize);
+      var expectMovies = (IEnumerable<Domain.Aggregates.Movie>)movies.GetRange(0, m_maxPageSize);
       var expectMoviesTask = Task.FromResult(expectMovies);
       m_movieRepositoryMock.Setup(m => m.GetAllAsync(m_maxPageSize, It.IsAny<int>()))
                            .Returns(expectMoviesTask);
@@ -170,19 +162,19 @@ namespace Dotflik.WebApp.Server.Services.UnitTests
     [InlineData(5)]
     [InlineData(30)]
     [InlineData(50)]
-    public async Task ListMovies_PageSizeIsBetweenZeroAndMaxPageSize_ReturnsRequestPageSizeMovies(int pageSize)
+    public async Task ListMovies_PageSizeIsBetweenOneAndMaxPageSize_ReturnsRequestPageSizeMovies(int pageSize)
     {
-      if (pageSize <= 0 || pageSize > m_maxPageSize)
+      if (pageSize < 1 || pageSize > m_maxPageSize)
       {
-        throw new ArgumentException($"{nameof(pageSize)} must be in the range [0, {m_maxPageSize}]");
+        throw new ArgumentException($"{nameof(pageSize)} must be in the range [1, {m_maxPageSize}]");
       }
 
       // Arrange
-      var movies = GenerateFakeMovies(m_maxPageSize);
+      var movies = GenerateFakeMovieAggrs(m_maxPageSize);
 
       var request = new PaginationRequest { PageSize = pageSize, PageToken = string.Empty };
 
-      var expectMovies = (IEnumerable<Domain.Entities.Movie>)movies.GetRange(0, pageSize);
+      var expectMovies = (IEnumerable<Domain.Aggregates.Movie>)movies.GetRange(0, pageSize);
       var expectMoviesTask = Task.FromResult(expectMovies);
       m_movieRepositoryMock.Setup(m => m.GetAllAsync(pageSize, It.IsAny<int>()))
                            .Returns(expectMoviesTask);
@@ -204,18 +196,18 @@ namespace Dotflik.WebApp.Server.Services.UnitTests
     [InlineData(1000, 3)]
     [InlineData(1000, 10)]
     [InlineData(2403, 45)]
-    [InlineData(9052, 20)]
+    [InlineData(5000, 20)]
     public async Task ListMovies_ConsecutiveRequestUntilPageTokenIsEmpty_TotalPagesMatch(int numMovies, int pageSize)
     {
       // Arrange
       var numPages = (int) Math.Ceiling(numMovies / (float)pageSize);
-      var movies = GenerateFakeMovies(numMovies);
+      var movies = GenerateFakeMovieAggrs(numMovies);
       var firstRequest = new PaginationRequest { PageSize = pageSize, PageToken = string.Empty };
 
       m_movieRepositoryMock.Setup(m => m.GetAllAsync(It.IsAny<int>(), It.IsAny<int>()))
                            .Returns<int, int>((limit, offset) => 
                            {
-                             IEnumerable<Domain.Entities.Movie> moviesRange;
+                             IEnumerable<Domain.Aggregates.Movie> moviesRange;
                              try
                              {
                                moviesRange = movies.GetRange(offset, limit);
@@ -256,26 +248,94 @@ namespace Dotflik.WebApp.Server.Services.UnitTests
 
     /// <summary>
     /// Generate <paramref name="size"/> number of fake
-    /// <see cref="Domain.Entities.Movie"/> objects
+    /// <see cref="Domain.Entities.Star"/> objects
+    /// </summary>
+    /// <param name="size">Number of fake star objects to generate</param>
+    /// <param name="seed">Random seed</param>
+    /// <returns><paramref name="size"/> number of star objects</returns>
+    private static List<Domain.Entities.Star> GenerateFakeStars(int size, int seed)
+    {
+      Func<int, string> createId = id => "nm" + $"{id}".PadLeft(7, '0');
+
+      var starId = 0;
+      var fakeStars = new Faker<Domain.Entities.Star>()
+                        .StrictMode(true)
+                        .RuleFor(m => m.Id, f => createId(starId++))
+                        .RuleFor(m => m.Name, f => f.Random.AlphaNumeric(50))
+                        .RuleFor(m => m.BirthYear, f => f.Random.Number(1990, 2021))
+                        .RuleFor(m => m.Headshot, f => f.Internet.Url());
+      
+      return fakeStars.UseSeed(seed).Generate(size);
+    }
+
+    /// <summary>
+    /// Generate <paramref name="size"/> number of fake
+    /// <see cref="Domain.Entities.Genre"/> objects
+    /// </summary>
+    /// <param name="size">Number of fake genre objects to generate</param>
+    /// <param name="seed">Random seed</param>
+    /// <returns><paramref name="size"/> number of genre objects</returns>
+    private static List<Domain.Entities.Genre> GenerateFakeGenres(int size, int seed)
+    {
+      string[] genreNames = new string[] 
+      {
+        "Action",
+        "Adult",
+        "Adventure",
+        "Animation",
+        "Biography",
+        "Comedy",
+        "Crime",
+        "Documentary",
+        "Drama",
+        "Family",
+        "Fantasy",
+        "History",
+        "Horror",
+        "Music",
+        "Musical",
+        "Mystery",
+        "Reality-TV",
+        "Romance",
+        "Sci-Fi",
+        "Sport",
+        "Thriller",
+        "War",
+        "Western"
+      };
+
+      var genreId = 0;
+      var fakeGenres = new Faker<Domain.Entities.Genre>()
+                        .StrictMode(true)
+                        .RuleFor(m => m.Id, f => genreId++)
+                        .RuleFor(m => m.Name, f => f.PickRandom(genreNames));
+
+      return fakeGenres.UseSeed(seed).Generate(size);
+    }
+
+    /// <summary>
+    /// Generate <paramref name="size"/> number of fake
+    /// <see cref="Domain.Aggregates.Movie"/> objects
     /// </summary>
     /// <param name="size">Number of fake movie objects to generate</param>
+    /// <param name="seed">Random seed</param>
     /// <returns><paramref name="size"/> number of movie objects</returns>
-    private static List<Domain.Entities.Movie> GenerateFakeMovies(int size)
+    private static List<Domain.Aggregates.Movie> GenerateFakeMovieAggrs(int size, int seed = RandomSeed)
     {
-      Randomizer.Seed = new Random(8675309);
-
       Func<int, string> createId = id => "tt" + $"{id}".PadLeft(7, '0');
 
       var movieId = 0;
-      var fakeMovies = new Faker<Domain.Entities.Movie>()
-                        .StrictMode(true)
-                        .RuleFor(m => m.Id,        f => createId(movieId++))
-                        .RuleFor(m => m.Title,     f => f.Random.AlphaNumeric(50))
-                        .RuleFor(m => m.Year,      f => f.Random.Number(1990, 2021))
-                        .RuleFor(m => m.Director,  f => f.Name.FullName())
-                        .RuleFor(m => m.BannerUrl, f => f.Internet.Url());
+      var fakerMovieAggrs = new Faker<Domain.Aggregates.Movie>()
+                            .StrictMode(true)
+                            .RuleFor(m => m.Id, f => createId(movieId++))
+                            .RuleFor(m => m.Title, f => f.Random.AlphaNumeric(50))
+                            .RuleFor(m => m.Year, f => f.Random.Number(1990, 2021))
+                            .RuleFor(m => m.Director, f => f.Name.FullName())
+                            .RuleFor(m => m.BannerUrl, f => f.Internet.Url())
+                            .RuleFor(m => m.Stars, f => GenerateFakeStars(f.Random.Number(1, 5), seed))
+                            .RuleFor(m => m.Genres, f => GenerateFakeGenres(f.Random.Number(1, 5), seed));
 
-      return fakeMovies.Generate(size);
+      return fakerMovieAggrs.UseSeed(seed).Generate(size);
     }
 
   }
